6bbb90477dead0c87c373836af30b6eb8b8748d1
meghdad <meghdad.iran7777@gmail.com>
2024-11-10 00:58:54 +0330

    new 2

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,31 @@
-const page = () => {
-	//make db call
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
 
-	return <p></p>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return <DesignConfigurator />;
 };
 
 export default page;
